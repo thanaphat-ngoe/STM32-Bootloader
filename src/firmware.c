f852235eedec7c4b855341b2a68701c9624fa990
Thanaphat Ngoennet <thanaphat.ngoe@gmail.com>
2025-10-10 14:09:20 +0700

    feature: Refactor

--- src/firmware.c
+++ src/firmware.c
@@ -1,22 +1,43 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
+
+#define CPU_FREQ (2000000)
+#define SYSTICK_FREQ (1000)
+
+volatile uint64_t ticks = 0;
+void sys_tick_handler(void) {
+    ticks++;
+    gpio_toggle(GPIOA, GPIO5);
+}
+
+static uint64_t get_ticks(void) {
+    return ticks;
+}
 
 static void gpio_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOA);
     gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 }
 
-static void delay(uint32_t count) {
-    for (uint32_t i = 0; i < count; i++) {
-        __asm__("nop");
-    }
+static void systick_setup(void) {
+    systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
 }
 
 int main(void) {
     gpio_setup();
+    systick_setup();
+
+    uint64_t start_time = get_ticks();
 
     while (1) {
-        gpio_toggle(GPIOA, GPIO5);
-        delay(50000);
+        if (get_ticks() - start_time >= SYSTICK_FREQ) {
+            start_time = get_ticks();
+        }
     }
+
+    return 0;
 }
